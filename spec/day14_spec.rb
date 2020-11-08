c40f37e20eb2eae82cfdc047e9a56ca0427015f2
Grégoire Seux <g.seux@criteo.com>
2020-11-08 14:23:54 +0100

    Better skeleton for specs
    Change-Id: I4ef28b20806a0bc83b55b209f8cf99587b60f269

--- spec/day14_spec.rb
+++ spec/day14_spec.rb
@@ -1,9 +1,49 @@
 # frozen_string_literal: true
 
-require 'day1'
+require 'day14'
 
-RSpec.describe Day1 do
-  it 'works' do
-    expect(true).to be(true)
+RSpec.describe Day14 do
+  subject { described_class.new(input: input, part: part) }
+  describe 'part 1' do
+    let(:part) { 1 }
+    {
+    }.each do |example, solution|
+      context "example #{example[0..15]}" do
+        let(:input) { example }
+        pending "works on example #{example}" do
+          expect(subject.solve).to eq(solution)
+        end
+      end
+    end
+
+    context 'real input' do
+      pending 'finds a solution for part1' do
+        solution = subject.solve
+        puts "Solution for part 1 is #{solution}"
+      end
+    end
+  end
+
+  describe 'part 2' do
+    let(:part) { 2 }
+    {
+    }.each do |example, solution|
+      context "example #{example[0..15]}" do
+        let(:input) { example }
+        pending "works on example #{example}" do
+          expect(subject.solve).to eq(solution)
+        end
+      end
+    end
+
+    context 'real input' do
+      pending 'finds a solution for part2' do
+        solution = subject.solve
+        puts "Solution for part 2 is #{solution}"
+      end
+    end
+  end
+
+  let(:input) do
   end
 end
