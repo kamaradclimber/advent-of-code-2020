af45f02bf3ed9e7910e8ee4cbbdb406105326c13
Grégoire Seux <g.seux@criteo.com>
2020-12-01 06:18:23 +0100

    Solve day 1

--- days/day1.rb
+++ days/day1.rb
@@ -3,4 +3,31 @@
 require 'aoc'
 
 class Day1 < Day
+  attr_reader :numbers
+
+  def solve_part1
+    @numbers = input.split("\n").map(&:to_i).sort
+    find_sum(2020)
+  end
+
+  def find_sum(target)
+    i = 0
+    j = numbers.size - 1
+    until numbers[i] + numbers[j] == target
+      if numbers[i] + numbers[j] > target
+        j -= 1
+      elsif numbers[i] + numbers[j] < target
+        i += 1
+      end
+      return if i == numbers.size || j == -1
+    end
+    numbers[i] * numbers[j]
+  end
+
+  def solve_part2
+    @numbers = input.split("\n").map(&:to_i).sort
+
+    third = numbers.find { |n| find_sum(2020 - n) }
+    find_sum(2020 - third) * third
+  end
 end
