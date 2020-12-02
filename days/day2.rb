c3cf2d83cf253b4bb6b145d1819dbf048ddef09e
Grégoire Seux <kamaradclimber@gmail.com>
2020-12-02 09:16:49 +0100

    Solve day2
    Change-Id: I511a73f2208416197916fb8b0859c90cb6a2937e

--- days/day2.rb
+++ days/day2.rb
@@ -3,4 +3,26 @@
 require 'aoc'
 
 class Day2 < Day
+  def solve_part1
+    input.split("\n").count do |line|
+      raise "Invalid line #{line}" unless line =~ /(\d+)-(\d+) (.): (.+)$/
+
+      letter = Regexp.last_match(3)
+      password = Regexp.last_match(4)
+      count = password.chars.count(letter)
+      (Regexp.last_match(1).to_i..Regexp.last_match(2).to_i).include?(count)
+    end
+  end
+
+  def solve_part2
+    input.split("\n").count do |line|
+      raise "Invalid line #{line}" unless line =~ /(\d+)-(\d+) (.): (.+)$/
+
+      letter = Regexp.last_match(3)
+      password = Regexp.last_match(4)
+      [Regexp.last_match(1).to_i, Regexp.last_match(2).to_i].one? do |pos|
+        password[pos - 1] == letter
+      end
+    end
+  end
 end
