c893ca94263893b47579189f4500d7dbbef3b1a7
Grégoire Seux <kamaradclimber@gmail.com>
2020-12-05 09:49:17 +0100

    Solve day5
    Change-Id: I9a4c3ff3e3eb81103aa5f78d4e717090c885b042

--- days/day5.rb
+++ days/day5.rb
@@ -3,4 +3,25 @@
 require 'aoc'
 
 class Day5 < Day
+  # @param pass [String]
+  # @return [Integer, Integer, Integer] [row, seat, seatid]
+  def boarding_pass(string)
+    seat_id = string.gsub(/(F|L)/, '0').gsub(/(B|R)/, '1').to_i(2)
+    row, column = seat_id.divmod(8)
+
+    # row & column seems useless in this problem!
+    [row, column, seat_id]
+  end
+
+  def solve_part1
+    input.split("\n").map { |pass| boarding_pass(pass)[2] }.max
+  end
+
+  def solve_part2
+    all_seats = input.split("\n").map { |pass| boarding_pass(pass)[2] }.sort
+
+    all_seats.each_cons(2).find do |seat_a, seat_b|
+      seat_a != seat_b - 1
+    end.first + 1
+  end
 end
