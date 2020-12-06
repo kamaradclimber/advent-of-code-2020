879c0111f19c18dd3530e2bea15f030f29ce5936
Grégoire Seux <kamaradclimber@gmail.com>
2020-12-06 06:10:27 +0100

    Solve day 6

--- days/day6.rb
+++ days/day6.rb
@@ -3,4 +3,20 @@
 require 'aoc'
 
 class Day6 < Day
+  def solve_part1
+    input.split("\n\n").map do |group|
+      group.gsub("\n", '').chars.uniq.size
+    end.sum
+  end
+
+  def solve_part2
+    input.split("\n\n").map do |group|
+      all_chars = group.gsub("\n", '').chars.uniq
+      all_chars.select do |char|
+        group.split("\n").all? do |line|
+          line.chars.include?(char)
+        end
+      end.size
+    end.sum
+  end
 end
