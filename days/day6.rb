8f44ba681369bbe5b06e534dfbba0c4e230bfabb
Grégoire Seux <kamaradclimber@gmail.com>
2020-12-06 06:18:39 +0100

    Slightly refactor day6 for speed

--- days/day6.rb
+++ days/day6.rb
@@ -2,21 +2,35 @@
 
 require 'aoc'
 
+module CharSet
+  refine String do
+    # uniq chars from a string without \n
+    def char_set
+      gsub("\n", '').chars.uniq
+    end
+  end
+end
+
 class Day6 < Day
+  using CharSet
   def solve_part1
-    input.split("\n\n").map do |group|
-      group.gsub("\n", '').chars.uniq.size
-    end.sum
+    groups.map(&:char_set).map(&:size).sum
   end
 
   def solve_part2
-    input.split("\n\n").map do |group|
-      all_chars = group.gsub("\n", '').chars.uniq
-      all_chars.select do |char|
-        group.split("\n").all? do |line|
-          line.chars.include?(char)
-        end
+    groups.map do |group|
+      forms = group.split("\n").map(&:chars)
+      group.char_set.select do |char|
+        forms.all? { |answers| answers.include?(char) }
       end.size
     end.sum
+  end
+
+  def groups
+    input.split("\n\n")
+  end
+
+  def char_set(group)
+    group.gsub("\n", '').chars.uniq
   end
 end
