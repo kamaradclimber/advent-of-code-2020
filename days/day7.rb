5ad13fedbe345d6bc0315a8d254e6880e588092b
Grégoire Seux <g.seux@criteo.com>
2020-12-07 09:01:34 +0100

    Solve day7
    Change-Id: I9a93c13f73fc4267766a14b8c42e46fbcfdb216f

--- days/day7.rb
+++ days/day7.rb
@@ -3,4 +3,56 @@
 require 'aoc'
 
 class Day7 < Day
+  TARGET = 'shiny gold'
+  def solve_part1
+    rules = parse_rules(input)
+
+    possible_bags = Set.new
+    solution_size = -1
+
+    while possible_bags.size != solution_size
+      solution_size = possible_bags.size
+      rules.each do |container, contained_bags|
+        possible_bags << container if contained_bags.keys.any? { |b| possible_bags.include?(b) || b == TARGET }
+      end
+    end
+    puts possible_bags.to_a.join(', ')
+    possible_bags.size
+  end
+
+  def solve_part2
+    rules = parse_rules(input)
+    counts = {}
+    count_bags(TARGET, rules, counts)
+  end
+
+  def count_bags(bag, rules, counts)
+    return counts[bag] if counts.key?(bag)
+
+    count = rules[bag].map do |contained_bag, quantity|
+      (1 + count_bags(contained_bag, rules, counts)) * quantity
+    end.sum
+    counts[bag] = count
+    count
+  end
+
+  def parse_rules(input)
+    rules = {}
+    input.split("\n").each do |line|
+      raise "Invalid line: #{line}" unless line =~ /(.+) bags contain (.+)\.$/
+
+      container_bag = Regexp.last_match(1)
+      raise "Multiple rules for #{container_bag}" if rules.key?(container_bag)
+
+      rules[container_bag] = {}
+      next if Regexp.last_match(2).strip == 'no other bags'
+
+      Regexp.last_match(2).split(', ').each do |contained_bag|
+        raise "Invalid contained bag: #{contained_bag}" unless contained_bag =~ /(\d+) (.+) bag(s)?/
+
+        rules[container_bag][Regexp.last_match(2)] = Regexp.last_match(1).to_i
+      end
+    end
+    rules
+  end
 end
--- spec/day7_spec.rb
+++ spec/day7_spec.rb
@@ -7,17 +7,26 @@ RSpec.describe Day7 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      'light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.' => 4
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -27,17 +36,33 @@ RSpec.describe Day7 do
   describe 'part 2' do
     let(:part) { 2 }
     {
+      'light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.' => 32,
+      'shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.' => 126
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part2' do
+      it 'finds a solution for part2' do
         solution = subject.solve
         puts "Solution for part 2 is #{solution}"
       end
@@ -45,5 +70,6 @@ RSpec.describe Day7 do
   end
 
   let(:input) do
+    File.read(File.join(File.dirname(__FILE__), 'day7_input.txt'))
   end
 end
