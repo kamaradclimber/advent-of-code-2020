f96f3b746286ed3394baa4dac0ffe67a3113442e
Grégoire Seux <g.seux@criteo.com>
2020-12-09 09:13:29 +0100

    Solve day 9
    Change-Id: I23e4373364dc318e2ec13e163312d345c2800aa2

--- days/day9.rb
+++ days/day9.rb
@@ -3,4 +3,42 @@
 require 'aoc'
 
 class Day9 < Day
+  def initialize(part:, input:, preamble_size: 25)
+    super(part: part, input: input)
+    @preamble_size = preamble_size
+  end
+
+  def solve_part1
+    numbers = input.split("\n").map(&:to_i)
+    preamble = numbers.take(@preamble_size)
+    numbers = numbers.drop(@preamble_size)
+    buffer = preamble
+    numbers.find do |n|
+      valid = buffer.combination(2).none? { |a, b| a + b == n }
+      buffer.shift
+      buffer << n
+      valid
+    end
+  end
+
+  def solve_part2
+    weak_point = solve_part1
+    numbers = input.split("\n").map(&:to_i)
+    i = 0
+    loop do
+      j = i + 1
+      set = Set.new
+      sum = numbers[i] + numbers[j]
+      set << numbers[i]
+      set << numbers[j]
+      while sum < weak_point
+        j += 1
+        sum += numbers[j]
+        set << numbers[j]
+      end
+      return set.max + set.min if sum == weak_point
+
+      i += 1
+    end
+  end
 end
