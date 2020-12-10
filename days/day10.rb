88b9252b84ef3a4ac20882bb04fb20ff99b92771
Grégoire Seux <g.seux@criteo.com>
2020-12-10 20:16:48 +0100

    Solve day10 part1
    part2 has a functional solution but too slow
    
    Change-Id: Ieb566953928770f5e7458bc9c069ad1e13129963

--- days/day10.rb
+++ days/day10.rb
@@ -3,4 +3,62 @@
 require 'aoc'
 
 class Day10 < Day
+  def solve_part1
+    chargers = input.split("\n").map(&:to_i).sort
+
+    builtin = chargers.max + 3
+
+    chargers << builtin
+
+    current_jolt = 0
+    differences = chargers.map do |charger|
+      difference = charger - current_jolt
+      raise "Cannot move from #{current_jolt} to #{charger}" if difference > 3
+
+      current_jolt = charger
+      difference
+    end.tally
+    differences[1] * differences[3]
+  end
+
+  def solve_part2
+    chargers = input.split("\n").map(&:to_i).sort
+    builtin = chargers.max + 3
+
+    to_test = []
+    to_test << chargers
+    valid_combinations = 0
+    puts "Min size #{builtin / 3}"
+    while to_test.any?
+      puts "Combinaisons to test #{to_test.size}, size #{to_test.first.size}"
+      next_to_test = Set.new
+      to_test
+        .select { |subset| find_path(0, subset, builtin) }
+        .tap { |subsets| valid_combinations += subsets.size }
+        .each do |subset|
+        children(subset).to_a.each do |s|
+          next_to_test << s
+        end
+      end
+      to_test = next_to_test
+    end
+    valid_combinations
+  end
+
+  def children(chargers)
+    chargers.combination(chargers.size - 1)
+  end
+
+  # return [FalseClass] false if there is no path with those chargers
+  def find_path(start, chargers, builtin)
+    chargers = chargers.dup
+    current_jolt = start
+    chargers << builtin
+    chargers.sort.each do |charger|
+      difference = charger - current_jolt
+      return false if difference > 3
+      current_jolt = charger
+    end
+    true
+  end
 end
--- spec/day10_spec.rb
+++ spec/day10_spec.rb
@@ -7,17 +7,59 @@ RSpec.describe Day10 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      '16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4' => 35,
+'28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3' => 220
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example[0..15].gsub("\n", ', ')}" do
+        it "works on example #{example[0..15].gsub("\n", ', ')}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -27,10 +69,52 @@ RSpec.describe Day10 do
   describe 'part 2' do
     let(:part) { 2 }
     {
+      '16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4' => 8,
+'28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3' => 19208
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example[0..15].gsub("\n", ', ')}" do
+        it "works on example #{example[0..15].gsub("\n", ', ')}" do
           expect(subject.solve).to eq(solution)
         end
       end
@@ -45,5 +129,99 @@ RSpec.describe Day10 do
   end
 
   let(:input) do
+    '54
+91
+137
+156
+31
+70
+143
+51
+50
+18
+1
+149
+129
+151
+95
+148
+41
+144
+7
+125
+155
+14
+114
+108
+57
+118
+147
+24
+25
+73
+26
+8
+115
+44
+12
+47
+106
+120
+132
+121
+35
+105
+60
+9
+6
+65
+111
+133
+38
+138
+101
+126
+39
+78
+92
+53
+119
+136
+154
+140
+52
+15
+90
+30
+40
+64
+67
+139
+76
+32
+98
+113
+80
+13
+104
+86
+27
+61
+157
+79
+122
+59
+150
+89
+158
+107
+77
+112
+5
+83
+58
+21
+2
+66'
   end
 end
