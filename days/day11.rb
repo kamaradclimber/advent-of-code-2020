6eb68a5c9724303d68591d867345ecd213e62203
Grégoire Seux <kamaradclimber@gmail.com>
2020-12-11 06:59:21 +0100

    WIP day11 part1

--- days/day11.rb
+++ days/day11.rb
@@ -3,4 +3,31 @@
 require 'aoc'
 
 class Day11 < Day
+  def solve_part1
+    boat = input.split("\n").map(&:chars)
+
+  end
+
+  class Boat
+    def initialize(grid)
+      @grid = grid
+    end
+
+    def get(x,y)
+      @grid[y][x]
+    end
+
+    def set(x,y, value)
+      @grid[y][x] = value
+    end
+
+    def neighbors(x,y)
+      [
+        [x-1,y],[x-1,y-1],[x-1,y+1],
+        [x,y-1],[x,y+1],
+        [x+1,y-1],[x+1,y],[x+1,y+1]
+      ].select { |a,b| a >= 0 && b >= 0 && a < grid.first.size && b < grid.size }
+        .map { |a,b| get(a,b) }
+    end
+  end
 end
