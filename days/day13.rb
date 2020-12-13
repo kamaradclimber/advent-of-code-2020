38126325ec255e6bb4e0fac3973a79c4accfb0f3
Grégoire Seux <g.seux@criteo.com>
2020-12-13 13:32:31 +0100

    Solve day 13
    Change-Id: I04b1817f93d303fcff15a6f30f71a8c3609c6bc4

--- days/day13.rb
+++ days/day13.rb
@@ -1,6 +1,47 @@
 # frozen_string_literal: true
 
 require 'aoc'
+require 'pry'
 
 class Day13 < Day
+  def solve_part1
+    lines = input.split("\n")
+    earliest_timestamp = lines.first.to_i
+    buses = lines[1].split(',').reject { |b| b == 'x' }.map(&:to_i)
+
+    bus_to_take = buses.min_by { |bus| earliest_start(earliest_timestamp, bus) }
+    (earliest_start(earliest_timestamp, bus_to_take) - earliest_timestamp) * bus_to_take
+  end
+
+  def earliest_start(now, bus)
+    (now / bus + 1) * bus
+  end
+
+  # @return 1/a mod b
+  # @raise RuntimeError if it's not possible
+  def invmod(a, b)
+    raise 'No inverse' unless a.gcd(b) == 1
+
+    c = 1
+    c += 1 while (c * a) % b != 1
+    c
+  end
+
+  def solve_part2
+    lines = input.split("\n")
+    buses = lines.last.split(',').each_with_index.map do |b, i|
+      next if b == 'x'
+
+      [b.to_i, b.to_i - i]
+    end.compact
+
+    n = buses.map(&:first).inject(:*)
+
+    series = buses.map do |m, r|
+      nhat = n / m
+      r * invmod(nhat, m) * nhat
+    end
+
+    series.sum % n
+  end
 end
--- spec/day13_spec.rb
+++ spec/day13_spec.rb
@@ -7,17 +7,19 @@ RSpec.describe Day13 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      '939
+7,13,x,x,59,x,31,19' => 295
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example[0..15].gsub("\n", ', ')}" do
+        it "works on example #{example[0..15].gsub("\n", ', ')}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -27,17 +29,23 @@ RSpec.describe Day13 do
   describe 'part 2' do
     let(:part) { 2 }
     {
+      '7,13,x,x,59,x,31,19' => 1068781,
+      '17,x,13,19' => 3417,
+      '67,7,59,61' => 754018,
+      '67,x,7,59,61' => 779210,
+      '67,7,x,59,61' => 1261476,
+      '1789,37,47,1889' => 1202161486
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example[0..15].gsub("\n", ', ')}" do
+        it "works on example #{example[0..15].gsub("\n", ', ')}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part2' do
+      it 'finds a solution for part2' do
         solution = subject.solve
         puts "Solution for part 2 is #{solution}"
       end
@@ -45,5 +53,7 @@ RSpec.describe Day13 do
   end
 
   let(:input) do
+    %(1000510
+19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,13,x,x,x,x,x,x,x,x,x,x,29,x,853,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23) # rubocop:disable Layout/LineLength
   end
 end
